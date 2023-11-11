1af07bc33d70e213fcffd34f8715e89ae8d6d6a6
Tyler Morgan <tyler@tylermorgan.me>
2023-11-11 15:19:16 -0800

    Modify velocity changes to be ref based

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { MutableRefObject, useMemo, useRef, useState } from 'react'
 import './App.css'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, PerspectiveCamera, Stats } from '@react-three/drei'
 import AxesHelperWithLabels from './components/AxesHelperWithLabels'
 import { useControls } from 'leva'
 import React from 'react'
-import { BallCollider, Physics, RigidBody, RigidBodyTypeString } from '@react-three/rapier'
+import { BallCollider, Physics, RapierRigidBody, RigidBody, RigidBodyTypeString, useRapier, vec3 } from '@react-three/rapier'
 import * as THREE from "three"
 
 interface Enemy {
@@ -16,21 +16,92 @@ interface Enemy {
 
 type Triplet = [x: number, y: number, z: number];
 
-const zeroVector = new THREE.Vector3(0, 0, 0);
+// const GameState = () => {
+//   const {world} = useRapier();
+//   useFrame(()=> {
+//     world.forEachRigidBody(body=> {
+//       body.setLinvel(new THREE.Vector3(1,0,0), false)
+//     })
+//   })
+
+//   return null
+// }
+
+interface EnemyRenderProps {
+  enemy: Enemy;
+  // sameDirection: boolean;
+  velocityMagnitude: number;
+  type: RigidBodyTypeString
+}
+
+
+const enabledTranslations: [boolean, boolean, boolean] = [true, false, true];
+
+interface CalcVelocityProps {
+  sameDirection: boolean;
+  velocityMagnitude: number;
+  velocity: Triplet;
+}
+
+const calcVelocity: (props: CalcVelocityProps) => Triplet = ({ sameDirection, velocityMagnitude, velocity }) => {
+  return sameDirection ?
+    [velocityMagnitude, 0, 0] :
+    [velocity[0] * velocityMagnitude, 0, velocity[2] * velocityMagnitude]
+}
+
+const RenderEnemy: React.FC<EnemyRenderProps> = ({
+  enemy,
+  // sameDirection,
+  // velocityMagnitude,
+  type
+}) => {
+  const bodyRef = useRef<RapierRigidBody>(null);
+  const vectorRef = useRef<THREE.Vector3>(new THREE.Vector3(0, 0, 0));
+  const enemyRef = useRef<Enemy>(enemy)
+  const counterRef = useRef(1);
+
+  useFrame(() => {
+    counterRef.current++
+    bodyRef.current?.setLinvel(
+      vectorRef.current.set(
+        enemyRef.current.velocity[0] * 0.0001 * counterRef.current,
+        0,
+        enemyRef.current.velocity[2] * 0.0001 * counterRef.current)
+      , true)
+  })
+
+
+  return (
+    <RigidBody
+      ref={bodyRef}
+      position={enemyRef.current.position}
+      canSleep={false}
+      lockRotations={true}
+      enabledTranslations={enabledTranslations}
+      type={type as RigidBodyTypeString}
+    >
+      <BallCollider
+        args={[0.25]}
+      />
+    </RigidBody>
+  )
+}
+
 
 function App() {
-  const [enemies, setEnemies] = useState<Enemy[]>([])
+  const [enemies, setEnemies] = useState<Enemy[]>([]);
   const { physicsDebug } = useControls({ physicsDebug: { value: true, label: "Physics Debug" } })
-  const { enemyCount: colliderCount } = useControls("Colliders", { enemyCount: { value: 100, min: 0, max: 10000, step: 100, label: "Count" } })
+  const { colliderCount } = useControls("Colliders", { colliderCount: { value: 500, min: 0, max: 3000, step: 100, label: "Count" } })
   const { spawnScale } = useControls("Colliders", { spawnScale: { value: 10, min: 0, max: 100, step: 100, label: "Spawn Scale" } })
 
   const { xGravity } = useControls("Gravity", { xGravity: { value: 0, min: -20, max: 20, step: 1, label: "X Gravity" } })
   const { zGravity } = useControls("Gravity", { zGravity: { value: 0, min: -20, max: 20, step: 1, label: "Y Gravity" } })
-  const { velocity } = useControls("Velocity", { velocity: { value: 0, min: -20, max: 20, step: 1, label: "Velocity" } })
+  const { velocityMagnitude } = useControls("Velocity", { velocityMagnitude: { value: 0, min: -20, max: 20, step: 1, label: "Magnitude" } })
+  const magnitudeRef = useRef(0);
   const { sameDirection } = useControls("Velocity", { sameDirection: { value: true, label: "Same Direction?" } })
   const { type } = useControls({
     type: {
-      options: ["dyanmic", "static", "kinematicPosition", "kinematicVelocity"],
+      options: ["dynamic", "static", "kinematicPosition", "kinematicVelocity"],
       label: "Rigid Body Type"
     }
   })
@@ -44,7 +115,6 @@ function App() {
     stabilizationIterations: { value: 1, min: 1, max: 2, step: 1, label: "Stabilization" }
   })
 
-
   React.useEffect(() => {
     const newEnemies: Enemy[] = []
     for (let i = 0; i < colliderCount; i++) {
@@ -53,15 +123,15 @@ function App() {
         position: [(-1 + Math.random() * 2) * spawnScale, 0, (-1 + Math.random() * 2) * spawnScale],
         velocity: [-1 + Math.random() * 2, 0, -1 + Math.random() * 2]
       })
-      setEnemies(newEnemies);
+
     }
+    setEnemies(newEnemies)
   }, [colliderCount, spawnScale])
 
   return (
     <Canvas>
       <OrbitControls />
       <PerspectiveCamera />
-
       <Stats />
       <AxesHelperWithLabels />
       <Physics
@@ -71,22 +141,14 @@ function App() {
         maxVelocityFrictionIterations={velocityFrictionIterations}
         maxStabilizationIterations={stabilizationIterations}
       >
-        {enemies.map(enemy => {
-          return (
-            <RigidBody
-              key={enemy.id}
-              position={enemy.position}
-              canSleep={false}
-              linearVelocity={sameDirection ? [velocity, 0, 0] : [enemy.velocity[0] * velocity, 0, enemy.velocity[2] * velocity]}
-              lockRotations={true}
-              type={type as RigidBodyTypeString}
-            >
-              <BallCollider
-                args={[0.25]}
-              />
-            </RigidBody>
-          )
-        })}
+        {/* <GameState/> */}
+        {enemies.map((enemy) => <RenderEnemy
+          key={enemy.id}
+          enemy={enemy}
+          // sameDirection={sameDirection}
+          // velocityMagnitude={velocityMagnitude}
+          type={type as RigidBodyTypeString}
+        />)}
       </Physics>
 
     </Canvas>
